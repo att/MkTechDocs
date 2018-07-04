3ef13b6a95dfffa6bed3ab8256a92865164541ba
jsseidel <jsseidel@fastmail.com>
2018-07-04 14:38:49 -0400

    Got rid of the no-groovy version because it didn't save that much space

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-cp Dockerfile.no-groovy Dockerfile
-docker build -t mktechdocs-no-groovy:0.0.1 .
-
-cp Dockerfile.groovy Dockerfile
-docker build -t mktechdocs-groovy:0.0.1 .
-
-rm -f Dockerfile
+docker build -t mktechdocs:0.0.1 .
