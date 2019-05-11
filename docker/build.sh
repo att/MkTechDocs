a47e51dcaf0ca3bb303a08d6fbd12df4a481ce95
Seidel, Joseph (js2589) <js2589@att.com>
2019-05-11 10:00:59 -0400

    Docker fix for https://github.com/att/MkTechDocs/issues/25

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker build -t mktechdocs:0.0.3 .
+docker build -t mktechdocs:0.0.4 .
+#docker build --no-cache -t mktechdocs:0.0.4 .
--- docker/docker-entry.sh
+++ docker/docker-entry.sh
@@ -4,8 +4,13 @@
   cd /project
   if [[ ! -r mktechdocs.conf ]] ; then
     mktechdocs init <<< $(echo y)
+    RV=$?
   else
     mktechdocs
+    RV=$?
   fi
+  exit $RV
 )
+RV=$? ; [[ $RV != 0 ]] && exit $RV
 
+exit 0
