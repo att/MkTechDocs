04f716b7f94359f3cf91447a81a5fa8b89027af1
jsseidel <jsseidel@fastmail.com>
2019-09-03 16:23:08 -0400

    Added import script

--- bin/markdown-2-mktechdocs.py
+++ bin/markdown-2-mktechdocs.py
@@ -15,9 +15,10 @@
  See the License for the specific language governing permissions and
  limitations under the License.
 
- This script imports a single markdown file, breaks it up into smaller
- markdown files by heading level, and then creates a MkTechDocs project from
- it.
+
+ This standalone script imports a single markdown file, breaks it up into
+ smaller markdown files by heading level, and then creates a MkTechDocs project
+ from it.
 
  Example runs:
 
@@ -49,38 +50,189 @@ import os
 import re
 import sys
 
+MKTECHDOCS_CONF = """TITLE=doc
+OUTPUT_FILE_NAME_BASE=doc
+FORMAT=cssframes
+HTML_STYLE=archwiki
+# Commented out so mktechdocs will not produce a TOC
+TABLE_OF_CONTENTS_MAIN_DEPTH=6
+TABLE_OF_CONTENTS_SUB_DEPTH=3
+SECTION_NUMBERS=true
+IMAGES=./img
+#BUILD_SCRIPT=postbuild.sh
+#CUSTOM_CSS=my.css
+#CUSTOM_TEMPLATE=mytemplate.html
+KEEP_TEMP_FILES=false
+#UML_OUTPUT_FORMAT=png
+"""
+
 def usage():
-    """ Usage output """
+    """Usage output"""
     print("%s <--file=F> [--heading-level=N] [--output-dir=D]" %
           os.path.basename(__file__))
     print("  -f, --file: input markdown file")
     print("  -h, --heading-level: Heading level to split files. Default: '1'.")
     print("  -o, --output-dir: The output directory. Default: '.'.")
 
+def is_desired_header(hline_1, hline_2, desired_level):
+    """
+    Analyzes line1 and line2 and desired_level (an integer) to determine if the
+    two lines represent an interesting level (i.e. match desired_level).
+
+    If true, returns the heading title as a string. If false, returns an empty
+    string.
+    """
+    # If hline_1 starts with a '#' character, we try to extract a title from
+    # it.
+    match_obj = re.search("^#{%s}[\\s]+(.*)" % desired_level, hline_1.rstrip())
+    if match_obj:
+        return match_obj.group(1)
+
+    # If desired level is 1 or 2, we need to do a lookahead since these can be:
+    #
+    #   heading 1
+    #   =========
+    #
+    #      or
+    #
+    #   heading 2
+    #   =========
+    #
+
+    level_indicator = "="
+    if int(desired_level) == 2:
+        level_indicator = "-"
+
+    match_obj = re.search("^[%s]+$" % level_indicator, hline_2.rstrip())
+    if match_obj:
+        return hline_1.rstrip()
+
+    return ""
+
+def is_line_all_header_1_or_2(line):
+    """Returns True if the given line is all '=' or all '-'."""
+    match_obj = re.search("^[=]+$", line.rstrip())
+    if match_obj:
+        return True
+
+    match_obj = re.search("^[\\-]+$", line.rstrip())
+    if match_obj:
+        return True
+
+    return False
+
+def heading_to_filename(heading):
+    """This function replaces whitespace, ., `, and () with hyphens. It then
+    lowercases the heading string and adds a markdown extension (.md)
+    """
+    filename = heading.lower()
+
+    # Replace bad chars
+    filename = re.sub("[\\s`\\.\\(\\)]+", "-", filename)
+
+    # Remove leading hyphens
+    filename = re.sub("^[\\-]", "", filename)
+
+    return "%s.md" % filename
+
 def do_import(markdown_file, heading_level, output_dir):
-    """ Imports the given markdown_file using the heading_level as the basis
+    """Imports the given markdown_file using the heading_level as the basis
     for where to split the file and puts output files in the given output_dir.
     """
     file_lines = []
 
-    # Read the given markdown file into an array. This is the easiest way to do
-    # a 1 line lookahead.
+    # Sanity check
     if not os.path.isfile(markdown_file):
         sys.stderr.write("%s doesn't exist. Giving up.\n" % markdown_file)
         sys.exit(1)
 
+    if not os.path.isdir(output_dir):
+        sys.stderr.write("%s doesn't exist. Giving up.\n" % output_dir)
+        sys.exit(1)
+
+    # Read the given markdown file into an array. This is the easiest way to do
+    # a 1 line lookahead.
     with open(markdown_file, "r") as my_file:
         file_lines = my_file.readlines()
 
-    max_idx = len(file_lines) - 1
-    idx = 0
+    # Open our master file to keep track of includes
+    master_file = open("%s/master.md" % output_dir, "w")
 
+    master_file.write("```include\n")
+
+    # Set up our line indices and initial values
+    max_idx = len(file_lines)
+    idx = 0
+    file_buffer = ""
+    heading = ""
     while idx < max_idx:
         # Grab 2 lines at a time
         line_1 = file_lines[idx]
         line_2 = "EOF"
         if (idx + 1) < max_idx:
             line_2 = file_lines[idx + 1]
+
+        title = is_desired_header(line_1, line_2, heading_level)
+
+        # If we get a valid title or we have reached the last line, we might
+        # have a stored heading to sort out
+        if title != "" or line_2 == "EOF":
+            # If we have a stored heading, we need to blast the collected lines
+            # underneath in a new file.
+            if heading != "":
+                # If the second line is EOF, we need to add the last line to
+                # the file buffer.
+                if line_2 == "EOF":
+                    file_buffer = "%s%s" % (file_buffer, line_1)
+
+                file_to_create = heading_to_filename(heading)
+                print("Creating file: %s/%s" % (output_dir, file_to_create))
+
+                # Add the heading to the file buffer
+                if heading_level in (1, 2):
+                    # Create the right number of underline characters
+                    underline = ""
+                    if heading_level == 1:
+                        underline = "=" * len(heading)
+                    elif heading_level == 2:
+                        underline = "-" * len(heading)
+
+                    file_buffer = "%s\n%s\n%s" % (heading, underline, file_buffer)
+                else:
+                    file_buffer = "%s\n%s" % (heading, file_buffer)
+
+                # Blast the new file
+                path = "%s/%s" % (output_dir, file_to_create)
+                with open(path, "w") as my_file:
+                    num_chars = my_file.write(file_buffer)
+                    if num_chars == 0:
+                        sys.stderr.write("WARNING: wrote 0 characters\n" % num_chars)
+
+                # Clear the file buffer
+                file_buffer = ""
+
+                # Record in master file
+                master_file.write("%s\n" % file_to_create)
+
+            # Save the heading we found for the next time
+            heading = title
+
+            # If line_2 is all '=' or '-', we can eat it
+            if is_line_all_header_1_or_2(line_2):
+                idx = idx + 1
+        else:
+            # We didn't find a heading, so save the line
+            file_buffer = "%s%s\n" % (file_buffer, line_1.rstrip())
+
+        # Advance the index
+        idx = idx + 1
+
+    # Close our master file include block
+    master_file.write("```\n")
+
+    # Create a new mktechdocs.conf file
+    conf_file = open("%s/mktechdocs.conf" % output_dir, "w")
+    conf_file.write(MKTECHDOCS_CONF)
 
 if __name__ == "__main__":
     ARGV = sys.argv[1:]
@@ -114,4 +266,4 @@ if __name__ == "__main__":
         usage()
         sys.exit(1)
 
-    do_import(ARG_FILE, ARG_LEVEL, ARG_DIR)
+    do_import(ARG_FILE, int(ARG_LEVEL), ARG_DIR)
